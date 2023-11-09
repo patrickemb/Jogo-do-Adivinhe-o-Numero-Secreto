2611c996ffeb7565cd8910586f83efb70ecdf67c
Patrick Cavalcante <patrickcorreacavalcante@gmail.com>
2023-11-09 09:29:40 -0300

    first commit

---
+++ app/validacao.js
@@ -0,0 +1,55 @@
+function verificaSeOChutePossuiUmValorValido(chute) {
+    const numero = +chute
+
+    if (chuteForInvalido(numero)) {
+        if (chuteForInvalido(numero)) {
+            if (chute.toUpperCase() === "GAME OVER") {
+    
+                document.body.innerHTML =
+                    `
+                    <h2>Game Over!!!</h2>
+                    <h3>Pressione o botão para jogar novamente</h3>
+                    <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                    `
+                    document.body.style.backgroundColor = "black";
+            } else {
+    
+                elementoChute.innerHTML += '<div>Valor Inválido</div>';
+            }
+        }
+    
+    }
+
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += `<br>valor inválido: Fale um número entre ${menorValor} e ${maiorValor}`
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `<h1>Você acertou!</h1>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML =+ `<div>O número secreto é menor<i class="fa-solid fa-down-long fa-bounce"></i></div>`
+
+    } else if (numero < numeroSecreto) {
+        elementoChute.innerHTML =+ `<div>O número secreto é maior<i class="fa-solid fa-up-long fa-bounce"></i></div>`
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+document.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload()
+    }
+})
